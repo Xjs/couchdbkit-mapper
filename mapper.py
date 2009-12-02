c4a41d8518df6d0c1cdb63dabe201f4febb42539
Jannis Andrija Schnitzer <jannis@myrkur.de>
2009-12-02 19:58:38 +0100

    Initially committing my previous progress.

---
+++ mapper.py
@@ -0,0 +1,279 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2009 Jannis Andrija Schnitzer <jannis.schnitzer@itisme.org>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+from couchdbkit.client import Database
+from couchdbkit.schema import SchemaProperties, Document
+
+from couchdbkit.schema.base import check_reserved_words
+from couchdbkit.schema import ReservedWordError
+
+# TODO: works only for new-style classes, need a way to find determine
+# old-style classes
+def coerce_doc(document, _TemporaryClass=None):
+    # The name was choosen in order not to conflict with the BIF `coerce'
+    """Coerce the given document into something (a new-style class or object)
+    that can be handled by CouchDBKit.
+    
+    Returns a 2-tuple of the new coerced class and the new document. The
+    latter is None if `document' was a class itself."""    
+    # `document' is either a class or an object of a class
+    # In the latter case, we need to create a new Object of a subclass
+    # of Document.
+    # We assume if `document' is a class if it has the __base__ 
+    # attribute.
+    
+    is_document = hasattr(document, '_db') # stolen from session.py
+    if is_document:
+        return document.__class__, document
+    
+    document_is_class = False
+    
+    if _TemporaryClass is None:
+        document_is_class = hasattr(document, '__base__')
+        if document_is_class:
+            old_class = document
+        else:
+            old_class = document.__class__
+        doc_type = old_class.__name__
+        
+        _TemporaryClass = SchemaProperties.__new__(SchemaProperties, doc_type, (Document, old_class), {})
+        
+    if not document_is_class:
+        try:
+            d = document.__dict__
+        except AttributeError:
+            d = document
+        document = object.__new__(_TemporaryClass)
+        Document.__init__(document, _d=d)
+        return (_TemporaryClass, document)
+    else:
+        return (_TemporaryClass, None)
+
+def dict_from_doc(doc):
+    try:
+        return doc.to_json()
+    except AttributeError:
+        return doc
+
+def bulk_inner(obj, docs, _raw_json=False):
+    """Return an iterator over the dictionaries of the given docs, needed
+    for the bulk actions"""
+    for doc in docs:
+        if _raw_json or isinstance(doc, dict):
+            yield doc
+        else:
+            yield obj.add(doc).to_json()
+            
+def map(database):
+    return Mapper(database.server, database.dbname)
+
+def document(method):
+    "Add the parent class's docstring to the inherited method"
+    # TODO: this is very unflexible :-(
+    method.__doc__ = getattr(Database, method.__name__).__doc__
+    return method
+
+class Mapper(Database):
+    """Provide high-level mapping of CouchDB documents to Python objects.
+    
+    Usage:
+        Call the mapper object with a class you'd like to map, or an object
+        that should be made into a Document-based object.
+        
+        Otherwise, this behaves like a Session object, except that everything
+        is mapped to Document-based (and your submitted) objects (i. e. view
+        results, got and set documents, etc.).
+    """
+    
+    # No WeakValueDictionary here, because sometimes you'd tell the mapper
+    # about classes to map without further references to them
+    classes = dict()
+        
+    def __init__(self, *args, **parameters):
+        super(Mapper, self).__init__(*args, **parameters)
+    
+    # obsolete
+    #def __call__(self, document):
+    #    return self.add(document)
+        
+    def add(self, document):
+        class_name = document.__class__.__name__
+        if class_name not in self.classes:
+            cls = None
+        else:
+            cls = self.classes[class_name]
+        if not isinstance(document, Document):
+            cls, document = coerce_doc(document, cls) # cls might be new now
+            class_name = cls.__name__
+        # sad that I have to check this twice, but I have no other chance
+        if class_name not in self.classes:
+            self.classes[class_name] = cls
+        if document is None:
+            document = cls
+        document.set_db(self)
+        return document
+    
+    def make_object(self, document):
+        """Create a Document-based object out of ``document'' (which can be a
+        dict or an object itself), using the previously mapped classes"""
+        def is_reserved_word(word):
+            try:
+                check_reserved_words(word)
+                # I think this is obsolete now
+                #return word == 'rev'
+                return False
+            except ReservedWordError:
+                return True
+        
+        if hasattr(document, '__dict__'):
+            class_name = document.__class__.__name__
+            document = document.__dict__
+        else:
+            try:
+                class_name = document['doc_type']
+            except KeyError:
+                class_name = None
+
+        filtered_dictionary = (
+            (key, value) for (key, value) in document.iteritems() \
+            if not is_reserved_word(key)
+        )
+
+        try:
+            cls = self.classes[class_name]
+        except KeyError:
+            cls = Document
+
+        assert(issubclass(cls, Document))
+        _, result = coerce_doc(
+            dict(filtered_dictionary), cls)
+        
+        try:
+            result['_id'] = document['_id']
+            result._doc['_rev'] = document['_rev']
+            # when benoit has finished his changes:
+            # result['_rev'] = document['_rev']
+        except KeyError:
+            pass
+        return result
+
+# somehow useless:        
+#    def contain(self, *docs):
+#        def contain_inner(docs):
+#            for doc in docs:
+#                yield self.add(doc)
+#        return super(Mapper, self).contain(*contain_inner(docs))
+    
+    def wrapper_maker(self, original_wrapper=None):
+        def mapper_wrapper(obj):
+            value = obj['value']
+            value['_id'] = obj['id'] # for make_object
+            if '_rev' not in value: # we might be the _all_docs view
+                try:
+                    value['_rev'] = obj['value']['rev']
+                    del(value['rev'])
+                except KeyError:
+                    pass
+            new_value = self.make_object(value)
+            obj['value'] = new_value
+            if callable(original_wrapper):
+                return original_wrapper(obj)
+            else:
+                return obj['value']
+        return mapper_wrapper
+
+    def view_wrapper(self, view_name, temporary_view=False, obj=None, wrapper=None, **parameters):
+        if obj is not None:
+            if not hasattr(obj, 'wrap') or not callable(obj.wrap):
+                raise AttributeError(''.join(["no 'wrap' method found in obj ", str(obj), "), or not callable"]))
+            else:
+                wrapper = obj.wrap
+        # clarity of code!
+        original_wrapper = wrapper
+        
+        if temporary_view:
+            view_func = super(Mapper, self).temp_view
+        else:
+            view_func = super(Mapper, self).view
+        return view_func(view_name, obj=None, wrapper=self.wrapper_maker(original_wrapper), **parameters)
+    
+    @document
+    def view(self, view_name, **parameters):
+        return self.view_wrapper(view_name, temporary_view=False, **parameters)
+    
+    @document
+    def temp_view(self, design, **parameters):
+        return self.view_wrapper(design, temporary_view=True, **parameters)
+        
+    @document
+    def get(self, docid, rev=None, wrapper=None, _raw_json=False):
+        def get_wrapper(obj):
+            if not _raw_json:
+                obj = self.make_object(obj)
+            if wrapper is not None:
+                if not callable(wrapper):
+                    raise TypeError("wrapper isn't a callable")
+                return wrapper(obj)
+            else:
+                return obj
+        return super(Mapper, self).get(docid, rev=rev, wrapper=get_wrapper, _raw_json=_raw_json)
+    
+    # what we inherit from Database
+    # copied and pasted, sad but true. But we need this special behavior
+    @document
+    def doc_revisions(self, docid, with_doc=True, _raw_json=False):
+        result = super(Mapper, self).doc_revisions(docid=docid, with_doc=with_doc, _raw_json=_raw_json)
+        if not with_doc or _raw_json:
+            return result
+        else:
+            return self.make_object(result)
+    
+    @document
+    def save_doc(self, doc, _raw_json=False, **params):
+        if hasattr(doc, '__dict__'): # Is not a dictionary itself
+            doc = self.add(doc)
+            doc.save()
+            return doc
+        else:
+            return super(Mapper, self).save_doc(doc, _raw_json=_raw_json, **params)
+    
+    @document
+    def __setitem__(self, doc_id, doc):
+        doc = self.add(doc)
+        return super(Mapper, self).__setitem__(doc_id, doc)
+    
+    @document
+    def bulk_save(self, docs, _raw_json=False, **parameters):
+        return super(Mapper, self).bulk_save(bulk_inner(self, docs, _raw_json), **parameters)
+    
+    @document
+    def bulk_delete(self, docs, **parameters):
+        return super(Mapper, self).bulk_delete(bulk_inner(self, docs, _raw_json), **parameters)
+    
+    @document
+    def delete_doc(self, doc, **parameters):
+        return super(Mapper, self).delete_doc(dict_from_doc(doc), **parameters)
+    
+    @document
+    def copy_doc(self, doc, dest=None, **parameters):
+        return super(Mapper, self).copy_doc(dict_from_doc(doc), dest=dest, **parameters)
+    
+    @document
+    def documents(self, wrapper=None, **parameters):
+        return self.view('_all_docs', wrapper=wrapper, **parameters)
+        
+    iterdocuments = documents
+    
---
+++ test_mapper.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2009 Benoit Chesneau <benoitc@e-engura.com> 
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+__author__ = 'jannis.schnitzer@itisme.org (Jannis Andrija Schnitzer)'
+
+from datetime import datetime
+import unittest
+
+from httplib2 import Http
+from restclient.transport import HTTPLib2Transport
+from restkit import ResourceNotFound
+
+from mapper import Mapper
+from couchdbkit.client import Server
+from couchdbkit.schema import Document
+
+class Greeting(object):
+    def __init__(self, author, content):
+        self.author  = author
+        self.content = content
+    def printout(self):
+        print self.author+':', self.content
+    def __eq__(self, other):
+        return self.author == other.author and self.content == other.content
+
+url = 'http://127.0.0.1:5984'
+user = None
+password = None
+db_name = 'couchdbkit_test'
+
+# alternatively
+url = 'http://localhost:5984'
+#user = 'username'
+#password = 'topsecret'
+#db_name = 'some_other_db'
+
+class MapperTestCase(unittest.TestCase):
+    def setUp(self):
+        if user is not None and password is not None:
+            http = Http()
+            http.add_credentials(user, password)
+            transport = HTTPLib2Transport(http=http)
+            transport.add_credentials(user, password)
+        else:
+            transport = None
+        self.server = Server(url, transport=transport)
+        self.server.create_db(db_name)
+        self.db = Mapper(self.server, db_name)
+        # TODO: create greeting/all view
+        design_doc = {
+            '_id': '_design/greeting',
+            'language': 'javascript',
+            'views': {
+                'all': {
+                    "map": """
+function (doc)
+{
+    if (doc.doc_type == "Greeting")
+        emit(doc._id, doc);
+}
+"""
+                }
+            }
+        }
+        self.db.save_doc(design_doc)
+    
+    def tearDown(self):
+        self.server.delete_db(db_name)        
+    
+    def testMapping(self):
+        # introduce our class to the mapper
+        self.db.add(Greeting)
+        self.assert_(issubclass(self.db.classes['Greeting'], (Greeting, Document)))
+        # TODO: more stuff
+        g = Greeting('Jannis', 'welcome to the mapping world')
+        self.db['g'] = g
+        mapped_g = self.db['g']
+        self.assert_(mapped_g._id == 'g')
+        self.assert_(isinstance(mapped_g, Greeting))
+        self.assert_(g == mapped_g)
+        greetings = [
+            Greeting('Jannis', 'this is the first test greeting'),
+            Greeting('Jannis again', 'this is the second test greeting')
+        ]
+        greetings[0]._id = 'first test greeting'
+        greetings[1]._id = 'second test greeting'
+        self.db.bulk_save(greetings)
+        self.assert_([self.db['first test greeting'],
+            self.db['second test greeting']] == greetings)
+        h = Greeting('Xjs', 'Hi!')
+        h2 = self.db.save_doc(h)
+        h2_id = h2._id
+        self.assertEquals(h, self.db[h2_id])
+        self.db.delete_doc(h2)
+        self.assertRaises(ResourceNotFound, self.db.__getitem__, h2_id)
+        self.db.copy_doc(mapped_g, 'another_g')
+        self.assertEquals(g, self.db['another_g'])
+    
+    def testViewResults(self):
+        def frobnicate(greeting):
+            """Helper function that returns a greeting which is ``frobnicated''
+            (this simulates an action being performed).
+            
+            Used as test for the wrapper func - because Mapper.view overwrites
+            the mapper func passed to Database.view with a custom one in order
+            to give back mapped objects but still retain the ability to pass
+            a custom wrapper function."""
+            greeting.frobnicated = True
+            return greeting
+            
+        # view generated in setUp
+        all_greetings = self.db.view('greeting/all', wrapper=frobnicate)
+        for greeting in all_greetings:
+            # Test if we get a Greeting class back, not a Document or even dict
+            self.assert_(isinstance(greeting, Greeting))
+            # Test if custom wrapper function worked though Mapper mapped the
+            # document (i. e. set a wrapper function itself)
+            self.assert_(greeting.frobnicated)
+    
+if __name__ == '__main__':
+    unittest.main()
+
